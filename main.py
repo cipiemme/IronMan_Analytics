a4c7215031afd3286f0a6ad071e20856b2a4879a
MarcelloZambelli <153529034+MarcelloZambelli@users.noreply.github.com>
2026-04-13 09:46:11 +0000

    Added demographics page and image o world heatmap in main page

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from custom import top_menu, bottom_head
 
 top_menu()
 
-
-text, map = st.columns([.35, .65])
-
-with map:
-    st.map()
+text, map = st.columns([.4, .6])
 
 with text:
     st.write("Qui ci vanno le parole")
+
+with map:
+    st.image("Images/world_map_colored_150.png", caption="Participants world heat map, for more info visit the [Demographics](demographics) page")
 
 bottom_head()
---
+++ pages/demographics.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from custom import top_menu, bottom_head
+
+top_menu()
+
+
+bottom_head()
+
+
