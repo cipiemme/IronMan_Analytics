54a3bbc003ec2ca99d164212469efccd119deaca
MarcelloZambelli <153529034+MarcelloZambelli@users.noreply.github.com>
2026-04-15 10:45:26 +0000

    Added hist plot in demographics page

--- pages/demographics.py
+++ pages/demographics.py
@@ -1,11 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 from custom import top_menu, bottom_head, load_merge
 
 top_menu()
 
 dem = load_merge()
+
+# how many from each category are present in dataset
+divisions = dem['Division'].unique()
+cat_counts = dem['Division'].value_counts().sort_index()
+cat_counts_df = pd.DataFrame([divisions, cat_counts])
+cat_counts_df = cat_counts_df.transpose()
+
+#create histogram plot
+dem_cat_hist_fig = px.histogram(cat_counts_df, x=0, y=1,
+                                labels={
+                                    "0": "Divisions",
+                                    "1": "# of participants"}
+                                    )
+
+st.plotly_chart(dem_cat_hist_fig)
+
+st.write(cat_counts_df)
+
 
 bottom_head()
 
